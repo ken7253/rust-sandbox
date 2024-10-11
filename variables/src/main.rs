a8aaa8e32ea6581cd3a502db7db1eafdc30e314c
ken7253 <ken7253@gmail.com>
2024-10-11 22:14:26 +0900

    Comment

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,4 +1,5 @@
 fn main() {
+    // https://doc.rust-jp.rs/book-ja/ch03-01-variables-and-mutability.html#%E5%A4%89%E6%95%B0%E3%81%A8%E5%8F%AF%E5%A4%89%E6%80%A7
     // 可変変数（？）の定義
     let mut x = 5;
     println!("The value of x is: {}", x); // xの値は{}です
@@ -11,6 +12,7 @@ fn main() {
     println!("The value of MAX_POINTS: {}", MAX_POINTS);
 
     // shadowing
+    // https://doc.rust-jp.rs/book-ja/ch03-01-variables-and-mutability.html#%E3%82%B7%E3%83%A3%E3%83%89%E3%83%BC%E3%82%A4%E3%83%B3%E3%82%B0
     let y = 5;
     // y => 5
     println!("The value of y is: {}", y);
