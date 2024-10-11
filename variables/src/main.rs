a2a30be73cd4744d58bd61379b6cba3e6cea381b
ken7253 <ken7253@gmail.com>
2024-10-11 22:11:36 +0900

    shadowing

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -9,4 +9,17 @@ fn main() {
     const MAX_POINTS: u32 = 100_000;
 
     println!("The value of MAX_POINTS: {}", MAX_POINTS);
+
+    // shadowing
+    let y = 5;
+    println!("The value of y is: {}", y);
+
+    let y = 6;
+    println!("The value of y is: {}", y);
+
+    {
+        let y = 10;
+
+        println!("The value of y is: {}", y);
+    }
 }
