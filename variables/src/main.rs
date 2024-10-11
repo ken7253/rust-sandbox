5aa723fe84a6431695d239dde058cf0d19f86814
ken7253 <ken7253@gmail.com>
2024-10-11 22:09:26 +0900

    const, comment

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,12 @@
 fn main() {
+    // 可変変数（？）の定義
     let mut x = 5;
     println!("The value of x is: {}", x); // xの値は{}です
     x = 6;
     println!("The value of x is: {}", x);
+
+    // 定数の宣言
+    const MAX_POINTS: u32 = 100_000;
+
+    println!("The value of MAX_POINTS: {}", MAX_POINTS);
 }
