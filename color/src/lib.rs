13067994eeb4c5af56cf971fbc6b4da3e45a3a1d
ken7253 <ken7253@gmail.com>
2024-10-13 19:13:28 +0900

    RGBをカラーコードに変換するメソッド

--- color/src/lib.rs
+++ color/src/lib.rs
@@ -8,6 +8,10 @@ impl Color {
     fn new(red: u8, green: u8, blue: u8) -> Self {
         Self { red, green, blue }
     }
+
+    fn to_hex(&self) -> String {
+        return format!("#{:X}{:X}{:X}", self.red, self.green, self.blue);
+    }
 }
 
 #[test]
@@ -17,4 +21,13 @@ fn initialize() {
     assert_eq!(color.red, 100);
     assert_eq!(color.green, 20);
     assert_eq!(color.blue, 50);
+}
+
+#[test]
+fn to_hex_color() {
+    let color = Color::new(255, 255, 255);
+    let hex = color.to_hex();
+
+    let correct = "#FFFFFF";
+    assert_eq!(hex, correct);
 }
