94b2b0a3b2e51c929157037d57360f61e512745c
ken7253 <ken7253@gmail.com>
2024-10-13 18:39:56 +0900

    色情報を扱う構造体を定義

--- color/src/lib.rs
+++ color/src/lib.rs
@@ -1,1 +1,20 @@
+struct Color {
+    red: u8,
+    green: u8,
+    blue: u8,
+}
 
+impl Color {
+    fn new(red: u8, green: u8, blue: u8) -> Self {
+        Self { red, green, blue }
+    }
+}
+
+#[test]
+fn initialize() {
+    let color = Color::new(100, 20, 50);
+
+    assert_eq!(color.red, 100);
+    assert_eq!(color.green, 20);
+    assert_eq!(color.blue, 50);
+}
