0b530853b2f65a7a2ba399b42cf3e0153e3524d5
ken7253 <ken7253@gmail.com>
2024-10-13 20:39:24 +0900

    反対色を求めるメソッドを定義

--- color/src/lib.rs
+++ color/src/lib.rs
@@ -1,3 +1,5 @@
+use std::u8;
+
 struct Color {
     red: u8,
     green: u8,
@@ -11,6 +13,16 @@ impl Color {
 
     fn to_hex(&self) -> String {
         return format!("#{:X}{:X}{:X}", self.red, self.green, self.blue);
+    }
+
+    fn reverse(&self) -> Color {
+        let reversed = Color::new(
+            u8::MAX - &self.red,
+            u8::MAX - &self.green,
+            u8::MAX - &self.blue,
+        );
+
+        return reversed;
     }
 }
 
@@ -30,4 +42,14 @@ fn to_hex_color() {
 
     let correct = "#FFFFFF";
     assert_eq!(hex, correct);
+}
+
+#[test]
+fn to_reverse_color() {
+    let color = Color::new(255, 255, 255);
+    let reverse = color.reverse();
+
+    assert_eq!(reverse.red, 0);
+    assert_eq!(reverse.green, 0);
+    assert_eq!(reverse.blue, 0);
 }
