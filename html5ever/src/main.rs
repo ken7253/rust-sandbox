e44bedf5d74804a8594f991b5ca089a758bb6d0e
ken7253 <ken7253@gmail.com>
2024-10-15 23:32:23 +0900

    トークナイズ処理を試す

--- html5ever/src/main.rs
+++ html5ever/src/main.rs
@@ -1,24 +1,95 @@
 extern crate html5ever;
-extern crate markup5ever_rcdom as rcdom;
 
-use html5ever::tendril::TendrilSink;
-use html5ever::tree_builder::TreeBuilderOpts;
-use html5ever::{parse_document, ParseOpts};
-use rcdom::RcDom;
+use html5ever::tendril::*;
+use html5ever::tokenizer::BufferQueue;
+use html5ever::tokenizer::{CharacterTokens, EndTag, NullCharacterToken, StartTag, TagToken};
+use html5ever::tokenizer::{
+    ParseError, Token, TokenSink, TokenSinkResult, Tokenizer, TokenizerOpts,
+};
+use std::cell::Cell;
 
+#[derive(Clone)]
+struct TokenPrinter {
+    in_char_run: Cell<bool>,
+}
+
+impl TokenPrinter {
+    fn is_char(&self, is_char: bool) {
+        match (self.in_char_run.get(), is_char) {
+            (false, true) => print!("CHAR : \""),
+            (true, false) => println!("\""),
+            _ => (),
+        }
+        self.in_char_run.set(is_char);
+    }
+
+    fn do_char(&self, c: char) {
+        self.is_char(true);
+        print!("{}", c.escape_default().collect::<String>());
+    }
+}
+
+impl TokenSink for TokenPrinter {
+    type Handle = ();
+
+    fn process_token(&mut self, token: Token, _line_number: u64) -> TokenSinkResult<()> {
+        match token {
+            CharacterTokens(b) => {
+                for c in b.chars() {
+                    self.do_char(c);
+                }
+            }
+            NullCharacterToken => self.do_char('\0'),
+            TagToken(tag) => {
+                self.is_char(false);
+                // This is not proper HTML serialization, of course.
+                match tag.kind {
+                    StartTag => print!("TAG  : <\x1b[32m{}\x1b[0m", tag.name),
+                    EndTag => print!("TAG  : <\x1b[31m/{}\x1b[0m", tag.name),
+                }
+                for attr in tag.attrs.iter() {
+                    print!(
+                        " \x1b[36m{}\x1b[0m='\x1b[34m{}\x1b[0m'",
+                        attr.name.local, attr.value
+                    );
+                }
+                if tag.self_closing {
+                    print!(" \x1b[31m/\x1b[0m");
+                }
+                println!(">");
+            }
+            ParseError(err) => {
+                self.is_char(false);
+                println!("ERROR: {}", err);
+            }
+            _ => {
+                self.is_char(false);
+                println!("OTHER: {:?}", token);
+            }
+        }
+        TokenSinkResult::Continue
+    }
+}
+
+/// In this example we implement the TokenSink trait in such a way that each token is printed.
+/// If a there's an error while processing a token it is printed as well.
 fn main() {
-    let opts = ParseOpts {
-        tree_builder: TreeBuilderOpts {
-            drop_doctype: true,
+    let sink = TokenPrinter {
+        in_char_run: Cell::new(false),
+    };
+
+    let mut tok = Tokenizer::new(
+        sink,
+        TokenizerOpts {
+            profile: true,
             ..Default::default()
         },
-        ..Default::default()
-    };
-    let data = "<!DOCTYPE html><html><body><a href=\"foo\"></a></body></html>".to_string();
-    let dom = parse_document(RcDom::default(), opts)
-        .from_utf8()
-        .read_from(&mut data.as_bytes())
-        .unwrap();
+    );
 
-    println!("{:?}", dom.document);
+    let mut input = BufferQueue::default();
+    input.push_back(format_tendril!(
+        r#"<!DOCTYPE html><html><body><a href=\"foo\"></a></body></html>"#
+    ));
+
+    let _ = tok.feed(&mut input);
 }
